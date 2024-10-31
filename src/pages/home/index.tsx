084c88406d2480e49eb1b7a844b6a7e7f4af8414
jaocruz <mcruznq@gmail.com>
2024-10-31 09:13:03 -0300

    mapeando issues na homepage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/header";
 import { HomeContainer, PostsContainer, SearchbarContainer } from "./styles";
 
-
 import { PostCard } from "./components/post-card";
 import { ProfileCard } from "./components/profile-card";
 
+import { api } from "../../lib/axios";
+import { useEffect, useState } from "react";
+
+interface IssueProps {
+   id: number
+   title: string
+   body: string
+   created_at: string
+}
+
 export function Home() {
+   const [issuesList, setIssuesList] = useState<IssueProps[]>([])
+
+   useEffect(() => {
+      async function fetchIssuesListInfo() {
+         const response = await api.get('https://api.github.com/search/issues?q=Boas%20pr%C3%A1ticas%20repo:rocketseat-education/reactjs-github-blog-challenge')
+   
+         const extractedData = response.data.items.map((item: any) => ({
+            id: item.id,
+            title: item.title,
+            body: item.body,
+            created_at: item.created_at,
+        }));
+
+         setIssuesList(extractedData)
+      }
+
+      fetchIssuesListInfo()
+   }, [])
+
    return (
       <>
       <Header />
@@ -22,14 +50,20 @@ export function Home() {
             <input type="text" placeholder="Buscar conteúdo"/>
          </SearchbarContainer>
 
-         <PostsContainer>
-            <PostCard />
-            <PostCard />
-            <PostCard />
-            <PostCard />
-            <PostCard />
-            <PostCard />
-         </PostsContainer>
+         { issuesList.length > 0 &&
+            <PostsContainer>
+               {issuesList.map(post => {
+                  return (
+                     <PostCard
+                     key={post.id}
+                     title={post.title}
+                     body={post.body}
+                     created_at={post.created_at}
+                     />
+                  )
+               })}
+            </PostsContainer>
+         }
       </HomeContainer>
       
       </>
